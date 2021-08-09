c3b0b989a0566bfea4fffcf64f9e31bacc2ae5f3
Vic-Fou <v.foulks0410@gmail.com>
2021-08-08 23:02:15 -0400

    project practice 2

--- Index.js
+++ Index.js
@@ -1,14 +1,16 @@
 const prompt= require("readline-sync");
 const wordBank = require("./Word-Bank.JSON")
 
-const letter = prompt.question("Please guess a letter")
+console.log("\nWelcome to Hang-Man!\nPress ctr+c to stop.\n")
 
-let chosenWord = wordBank[math.floor[math.random()*wordBank.length]];
+const letter = prompt.question("Please guess a letter: ");
+
+let chosenWord = wordBank[Math.floor[Math.random()*wordBank.length]];
 let answerArray=[""];
 for(let i=0;i<chosenWord.length;i++){
     answerArray[i]="_";
 };
 console.log(letter)
 
-//console.log(wordBank[0]);
+console.log(wordBank[0]);
 //console.log('YOU WON!');
