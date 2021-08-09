5db9a67c4917f492091e7dde6e6fe7ac95d82226
Vic-Fou <v.foulks0410@gmail.com>
2021-08-09 05:50:57 -0400

    Halfway done project

--- Index.js
+++ Index.js
@@ -1,16 +1,39 @@
 const prompt= require("readline-sync");
-const wordBank = require("./Word-Bank.JSON")
+const wordBank= ["fish","punch","liquid","fudge","slay","magic","run","fun","coding","program","computer","algorithim","equation","chicken","boredom"];//require("./Word-Bank.JSON");
 
-console.log("\nWelcome to Hang-Man!\nPress ctr+c to stop.\n")
+console.log("\nWelcome to Hang-Man!\nPress ctr+c to end game.\n");
 
-const letter = prompt.question("Please guess a letter: ");
+let chosenWord= wordBank[Math.floor(Math.random()*wordBank.length)];
 
-let chosenWord = wordBank[Math.floor[Math.random()*wordBank.length]];
-let answerArray=[""];
+/*let parsedWordBank = JSON.parse(JSON.stringify(wordBank[""]));
+let chosenWord= parsedWordBank[Math.floor(Math.random()*parsedWordBank.length)];*/
+
+//console.log(chosenWord);
+
+let answerArray=[];
 for(let i=0;i<chosenWord.length;i++){
     answerArray[i]="_";
 };
-console.log(letter)
+let letterRemainder=chosenWord.length;
 
-console.log(wordBank[0]);
+
+while(letterRemainder>0){
+    console.log(answerArray.join(" "));
+    const letterGuessed = prompt.question("Please guess a letter: ");
+    if (letterGuessed.length!==1){
+        console.log("Please enter one letter.");
+    }
+    else if(letterGuessed===null){
+        break;
+    }
+    else{
+        for(let j=0;j<chosenWord.length; j++){
+        if (chosenWord[j]===letterGuessed){
+            answerArray[j] = letterGuessed;
+            letterRemainder--;
+            }
+        }
+    }
+}
+//console.log(wordBank[0]);
 //console.log('YOU WON!');
