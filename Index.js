28d2f325bdd11fbd287b4e8ccc6a5b8bdbccc77b
Vic-Fou <v.foulks0410@gmail.com>
2021-08-08 23:00:52 -0400

    project practice

--- Index.js
+++ Index.js
@@ -3,6 +3,12 @@ const wordBank = require("./Word-Bank.JSON")
 
 const letter = prompt.question("Please guess a letter")
 
+let chosenWord = wordBank[math.floor[math.random()*wordBank.length]];
+let answerArray=[""];
+for(let i=0;i<chosenWord.length;i++){
+    answerArray[i]="_";
+};
+console.log(letter)
+
 //console.log(wordBank[0]);
-console.log('YOU WON!\n
-You beat 3 out of 5 rounds');
+//console.log('YOU WON!');
