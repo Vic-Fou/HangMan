5cbba1b05ddf74d02c0350488fa57a904e41098e
Vic-Fou <v.foulks0410@gmail.com>
2021-08-14 15:15:46 -0400

    more Hangman Progress

--- Index.js
+++ Index.js
@@ -17,8 +17,13 @@ for(let i=0;i<chosenWord.length;i++){
 let letterRemainder=chosenWord.length;
 
 
-while(letterRemainder>0){
-    console.log(answerArray.join(" "));
+
+let guess = 0;
+let wrongGuesses = 0;
+while(letterRemainder>0 && wrongGuesses <= 6) {
+    //console.log(answerArray.join(" "));
+    console.log(wrongGuesses,"wrong guesses")
+
     const letterGuessed = prompt.question("Please guess a letter: ");
     if (letterGuessed.length!==1){
         console.log("Please enter one letter.");
@@ -26,14 +31,44 @@ while(letterRemainder>0){
     else if(letterGuessed===null){
         break;
     }
+    //this needs to be cleaned up so that the wrongGuesses won't increment with the right answer
+    //Also the Hangman and gameover should only show if the player is doing wrong
     else{
+        if (!chosenWord.includes(answerArray)){
+            console.log("\n O\nTry a different letter");
+            wrongGuesses++;
+            if (wrongGuesses===2){
+                console.log(" O\n/");
+            }
+            else if(wrongGuesses===3){
+                console.log(" O\n/|");
+            }
+            else if(wrongGuesses===4){
+                console.log(" O\n/|)");
+            }
+            else if(wrongGuesses===5){
+                console.log(" O\n/|)\n/");
+            }
+            else if(wrongGuesses===6){
+                console.log(" O\n/|)\n/ |\noh no! Gameover.")
+                break
+            }
+             
+        }
+    }
         for(let j=0;j<chosenWord.length; j++){
         if (chosenWord[j]===letterGuessed){
             answerArray[j] = letterGuessed;
             letterRemainder--;
-            }
+            console.log(letterRemainder);
+           }  
+
         }
-    }
-}
+    
 console.log(answerArray.join(" "));
 console.log("Good Job, The answer was "+chosenWord);
+}
+// Don't forget to include arrow functions
+
+
+
