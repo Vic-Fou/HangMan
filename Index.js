f88513682f539468007b3e902030d74b3ce48f61
Vic-Fou <v.foulks0410@gmail.com>
2021-08-16 23:47:40 -0400

    First Draft

--- Index.js
+++ Index.js
@@ -15,16 +15,25 @@ const wordBank = [
   "equation",
   "chicken",
   "boredom",
+  "populace",
+  "amazing",
+  "grace",
+  "finesse",
+  "unconditional",
+  "own",
+  "object",
+  "permanent",
+  "data",
 ]; //require("./Word-Bank.JSON");
 
 console.log("\nWelcome to Hang-Man!\nPress ctr+c to end game.\n");
 
 let chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log(chosenWord)
+console.log(chosenWord);
 let chosenWord2 = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log(chosenWord2)
+console.log(chosenWord2);
 let chosenWord3 = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log(chosenWord3)
+console.log(chosenWord3);
 
 /*let parsedWordBank = JSON.parse(JSON.stringify(wordBank[""]));
 let chosenWord= parsedWordBank[Math.floor(Math.random()*parsedWordBank.length)];*/
@@ -57,165 +66,167 @@ let letterRemainder3 = chosenWord3.length;
 let wrongGuesses = 0;
 let wrongGuesses2 = 0;
 let wrongGuesses3 = 0;
-let numberOfRounds =1;
+let numberOfRounds = 1;
 
 //increment rounds toward the end , then console.log them to show the player which they are on
 
- while (letterRemainder3>0 && wrongGuesses <= 6 && numberOfRounds <= 4){;
- console.log("*Round" + " " + numberOfRounds + "" + "*\n");
- console.log(answerArray.join(" "));
- console.log(wrongGuesses, "wrong guesses");
+while (letterRemainder3 > 0 && wrongGuesses <= 6 && numberOfRounds <= 4) {
+  console.log("*Round" + " " + numberOfRounds + "" + "*\n");
+  console.log(answerArray.join(" "));
+  console.log(wrongGuesses, "wrong guesses");
 
- const letterGuessed = prompt.question("Please guess a letter: ");
- if (letterGuessed.length !== 1) {
+  let letterGuessed = prompt.question("Please guess a letter: ");
+  if (letterGuessed.length !== 1) {
+    console.log("Please enter letters.");
+  } else if (letterGuessed === null) {
+    return;
+  } else if (!/^[a-zA-Z]*$/g.test(letterGuessed)) {
+    console.log("\nInvalid.\nPlease enter letters.");
+  } else if (/^[A-Z]*$/g.test(letterGuessed)) {
+    letterGuessed === letterGuessed.toLowerCase();
+    console.log("\nPlease type in lowercase temporarily\n (awaiting patch)");
+  } else if (!chosenWord.includes(letterGuessed)) {
+    console.log("\n O\nIncorrect.Try a different letter");
+    wrongGuesses++;
 
-   console.log("Please enter letters.");
+    if (wrongGuesses === 2) {
+      console.log(" O\n/");
+    } else if (wrongGuesses === 3) {
+      console.log(" O\n/|");
+    } else if (wrongGuesses === 4) {
+      console.log(" O\n/|)");
+    } else if (wrongGuesses === 5) {
+      console.log(" O\n/|)\n/");
+    } else if (wrongGuesses === 6) {
+      console.log(
+        " O\n/|)\n/ |\noh no! The word was " + chosenWord + "\nGameover."
+      );
+      return;
+    }
+  }
+  //this needs to be cleaned up so that the wrongGuesses won't increment with the right answer
+  //Also the Hangman and gameover should only show if the player is doing wrong
+  //Done
+  else {
+    for (let j = 0; j < chosenWord.length; j++) {
+      if (chosenWord[j] === letterGuessed) {
+        answerArray[j] = letterGuessed;
+        letterRemainder--;
+        console.log(letterRemainder);
+      } else if (letterRemainder === 0) continue;
+    }
+  }
+  //find a way to track rounds then increment them to make the while loop repeat
+  //use the new asssets
+  while (letterRemainder === 0) {
+    console.log("Good Job, The answer was " + chosenWord);
 
- } else if (letterGuessed === null) {
+    let numberOfRounds = 2;
+    console.log("*Round" + " " + numberOfRounds + "" + "*\n");
 
-   return;
+    if (numberOfRounds === 2) {
+      console.log(answerArray2.join(" "));
+      console.log(wrongGuesses2, "wrong guesses");
 
- } else if (!/^[a-zA-Z]*$/g.test(letterGuessed)){
-    console.log("\nInvalid.\nPlease enter letters.")
-
- } else if (!chosenWord.includes(letterGuessed)) {
-
-   console.log("\n O\nIncorrect.Try a different letter");
-   wrongGuesses++;
-
-   if (wrongGuesses === 2) {
-     console.log(" O\n/");
-   } else if (wrongGuesses === 3) {
-     console.log(" O\n/|");
-   } else if (wrongGuesses === 4) {
-     console.log(" O\n/|)");
-   } else if (wrongGuesses === 5) {
-     console.log(" O\n/|)\n/");
-   } else if (wrongGuesses === 6) {
-     console.log(
-       " O\n/|)\n/ |\noh no! The word was " + chosenWord + "\nGameover."
-     );
-     return;
-   }
- }
- //this needs to be cleaned up so that the wrongGuesses won't increment with the right answer
- //Also the Hangman and gameover should only show if the player is doing wrong
- //Done
- else {
-   for (let j = 0; j < chosenWord.length; j++) {
-     if (chosenWord[j] === letterGuessed) {
-       answerArray[j] = letterGuessed;
-       letterRemainder--;
-       console.log(letterRemainder);
-     }
-     else if (letterRemainder===0) continue;
-     ;
-   }
- }
- //find a way to track rounds then increment them to make the while loop repeat
- //use the new asssets
- while (letterRemainder === 0) {
-   console.log("Good Job, The answer was " + chosenWord);
-   
-   let numberOfRounds=2;
-   console.log("*Round" + " " + numberOfRounds + "" + "*\n");
-   
-   if (numberOfRounds === 2) {
-     console.log(answerArray2.join(" "));
-     console.log(wrongGuesses2, "wrong guesses");
-
-     const letterGuessed2 = prompt.question("Please guess a letter: ");
-     if (letterGuessed2.length !== 1) {
-       console.log("Please enter one letter.");
-     } else if (letterGuessed2 === null) {
-       return;
-     }else if (!/^[a-zA-Z]*$/g.test(letterGuessed)){
-      console.log("\nInvalid.\nPlease enter letters.")
-  
-    }else if (!chosenWord2.includes(letterGuessed2)) {
-       console.log("\n O\nIncorrect.Try a different letter");
-       wrongGuesses2++;
-       if (wrongGuesses2 === 2) {
-         console.log(" O\n/");
-       } else if (wrongGuesses2 === 3) {
-         console.log(" O\n/|");
-       } else if (wrongGuesses2 === 4) {
-         console.log(" O\n/|)");
-       } else if (wrongGuesses2 === 5) {
-         console.log(" O\n/|)\n/");
-       } else if (wrongGuesses2 === 6) {
-         console.log(
-           " O\n/|)\n/ |\noh no! The word was " + chosenWord2 + "\nGameover."
-         );
-         return;
-       }
-     }
-
-     else {
-       for (let m = 0; m < chosenWord2.length; m++) {
-         if (chosenWord2[m] === letterGuessed2) {
-           answerArray2[m] = letterGuessed2;
-           letterRemainder2--;
-           console.log(letterRemainder2);
-         }
-         else if (letterRemainder2===0) continue;
-       }
-     }//}
-   while (letterRemainder2===0){
-       
-       console.log("Good Job,the answer was "+ chosenWord2);
-       let numberOfRounds=3;
-       console.log("*Round" + " " + numberOfRounds + "" + "*\n");
-       
-       if (numberOfRounds === 3) {
-        console.log(answerArray3.join(" "));
-        console.log(wrongGuesses3, "wrong guesses");
-   
-        const letterGuessed3 = prompt.question("Please guess a letter: ");
-        if (letterGuessed3.length !== 1) {
-          console.log("Please enter one letter.\n");
-        } else if (letterGuessed3 === null) {
+      let letterGuessed2 = prompt.question("Please guess a letter: ");
+      if (letterGuessed2.length !== 1) {
+        console.log("Please enter one letter.");
+      } else if (/^[A-Z]*$/g.test(letterGuessed2)) {
+        letterGuessed2 === letterGuessed2.toLowerCase();
+        console.log(
+          "\nPlease type in lowercase temporarily\n (awaiting patch)"
+        );
+      } else if (letterGuessed2 === null) {
+        return;
+      } else if (!/^[a-zA-Z]*$/g.test(letterGuessed)) {
+        console.log("\nInvalid.\nPlease enter letters.");
+      } else if (!chosenWord2.includes(letterGuessed2)) {
+        console.log("\n O\nIncorrect.Try a different letter");
+        wrongGuesses2++;
+        if (wrongGuesses2 === 2) {
+          console.log(" O\n/");
+        } else if (wrongGuesses2 === 3) {
+          console.log(" O\n/|");
+        } else if (wrongGuesses2 === 4) {
+          console.log(" O\n/|)");
+        } else if (wrongGuesses2 === 5) {
+          console.log(" O\n/|)\n/");
+        } else if (wrongGuesses2 === 6) {
+          console.log(
+            " O\n/|)\n/ |\noh no! The word was " + chosenWord2 + "\nGameover."
+          );
           return;
-        }else if (!/^[a-zA-Z]*$/g.test(letterGuessed)){
-         console.log("\nInvalid.\nPlease enter letters.\n")
-     
-       }else if (!chosenWord3.includes(letterGuessed3)) {
-          console.log("\n O\nIncorrect.Try a different letter\n");
-          wrongGuesses3++;
-          if (wrongGuesses3 === 2) {
-            console.log(" O\n/");
-          } else if (wrongGuesses3 === 3) {
-            console.log(" O\n/|");
-          } else if (wrongGuesses3 === 4) {
-            console.log(" O\n/|)");
-          } else if (wrongGuesses3 === 5) {
-            console.log(" O\n/|)\n/");
-          } else if (wrongGuesses3 === 6) {
-            console.log(
-              " O\n/|)\n/ |\noh no! The word was " + chosenWord3 + "\nGameover."
-            );
+        }
+      } else {
+        for (let m = 0; m < chosenWord2.length; m++) {
+          if (chosenWord2[m] === letterGuessed2) {
+            answerArray2[m] = letterGuessed2;
+            letterRemainder2--;
+            console.log(letterRemainder2);
+          } else if (letterRemainder2 === 0) continue;
+        }
+      } //}
+      while (letterRemainder2 === 0) {
+        console.log("Good Job,the answer was " + chosenWord2);
+        let numberOfRounds = 3;
+        console.log("*Round" + " " + numberOfRounds + "" + "*\n");
+
+        if (numberOfRounds === 3) {
+          console.log(answerArray3.join(" "));
+          console.log(wrongGuesses3, "wrong guesses");
+
+          let letterGuessed3 = prompt.question("Please guess a letter: ");
+          if (letterGuessed3.length !== 1) {
+            console.log("Please enter one letter.\n");
+          } else if (letterGuessed3 === null) {
             return;
-          }
-        }
-   
-        else {
-          for (let n = 0; n < chosenWord3.length; n++) {
-            if (chosenWord3[n] === letterGuessed3) {
-              answerArray3[n] = letterGuessed3;
-              letterRemainder3--;
-              console.log(letterRemainder3);
+          } else if (!/^[a-zA-Z]*$/g.test(letterGuessed)) {
+            console.log("\nInvalid.\nPlease enter letters.\n");
+          } else if (/^[A-Z]*$/g.test(letterGuessed3)) {
+            letterGuessed3 === letterGuessed3.toLowerCase();
+            console.log(
+              "\nPlease type in lowercase temporarily\n (awaiting patch)"
+            );
+          } else if (!chosenWord3.includes(letterGuessed3)) {
+            console.log("\n O\nIncorrect.Try a different letter\n");
+            wrongGuesses3++;
+            if (wrongGuesses3 === 2) {
+              console.log(" O\n/");
+            } else if (wrongGuesses3 === 3) {
+              console.log(" O\n/|");
+            } else if (wrongGuesses3 === 4) {
+              console.log(" O\n/|)");
+            } else if (wrongGuesses3 === 5) {
+              console.log(" O\n/|)\n/");
+            } else if (wrongGuesses3 === 6) {
+              console.log(
+                " O\n/|)\n/ |\noh no! The word was " +
+                  chosenWord3 +
+                  "\nGameover."
+              );
+              return;
             }
-            if (letterRemainder3===0){
-              console.log("Good Job,The answer was "+ chosenWord3+"\nCongratulations!!!\n**YOU WON**\nPress ctr+c to exit out of the game")
-              break
-            } 
-              ;
+          } else {
+            for (let n = 0; n < chosenWord3.length; n++) {
+              if (chosenWord3[n] === letterGuessed3) {
+                answerArray3[n] = letterGuessed3;
+                letterRemainder3--;
+                console.log(letterRemainder3);
+              }
+              if (letterRemainder3 === 0) {
+                console.log(
+                  "Good Job,The answer was " +
+                    chosenWord3 +
+                    "\nCongratulations!!!\n**YOU WON**\nPress ctr+c to exit out of the game"
+                );
+                break;
+              }
+            }
           }
         }
-   }
-}
-}
-}
+      }
+    }
+  }
 }
 //console.log(answerArray.join(" "));
 
